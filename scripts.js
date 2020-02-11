42d214102f8bb6754860df876bae6788ddd0b4c3
Karabo Mantse <57215840+Karabo9723@users.noreply.github.com>
2020-02-11 06:13:36 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const card = document.querySelectorAll(".memory-card");
+
+function flipCard() {
+    this.classList.toggle("flip");
+  }
+  
+cards.forEach(card => card.addEventListener("click", flipCard));
